3958ddae57f5c8ec35e2d2353b76e7ea4aff39f1
HaddajiDev <ahmedhaddajiahmed@gmail.com>
2025-01-24 19:04:24 +0100

    update metadata

--- api/index.js
+++ api/index.js
@@ -8,7 +8,7 @@ const app = express();
 const port = 5000;
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: process.env.BACKEND_URL,
   credentials: true
 }));
 app.use(express.json());
